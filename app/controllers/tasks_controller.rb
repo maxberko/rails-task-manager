37a82151c23be63a8f422669cd520b066c0053a9
maxime berkowicz <maximeberkowicz@gmail.com>
2020-08-04 14:47:37 +0200

    solution

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    @task = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    new_task = Task.new(task_params)
+    new_task.save
+    redirect_to task_path(new_task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show'
+  resources :tasks
 end
