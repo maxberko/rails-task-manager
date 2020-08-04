3ad936dc288e15167218dac8d25535812ee63643
maxime berkowicz <maximeberkowicz@gmail.com>
2020-08-04 16:05:42 +0200

    solution

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,24 @@ class TasksController < ApplicationController
     redirect_to task_path(new_task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
